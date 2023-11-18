26b94d5e035071351a36083a68aedcada3165878
chagchagchag <chagchagchag.dev@gmail.com>
2023-11-19 08:44:50 +0900

    order-service
    spring kafka test 의존성 추가

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 }
 
 extra["springCloudVersion"] = "2022.0.4"
+extra["testcontainersVersion"] = "1.18.3"
 
 subprojects{
     apply(plugin = "org.jetbrains.kotlin.jvm")
@@ -45,13 +46,18 @@ allprojects{
         implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.springframework.kafka:spring-kafka")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
+        testImplementation("org.springframework.kafka:spring-kafka-test")
+        testImplementation("org.testcontainers:junit-jupiter")
+        testImplementation("org.testcontainers:kafka")
     }
 
     dependencyManagement {
         imports {
             mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+            mavenBom("org.testcontainers:testcontainers-bom:${property("testcontainersVersion")}")
         }
     }
 
