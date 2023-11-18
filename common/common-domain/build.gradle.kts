16bb87eddd69197d316335e8c9baabe97d838ec6
chagchagchag <chagchagchag.dev@gmail.com>
2023-11-18 19:07:37 +0900

    common:common-domain

--- common/common-domain/build.gradle.kts
+++ common/common-domain/build.gradle.kts
@@ -1,3 +1,4 @@
 dependencies {
     implementation("com.github.f4b6a3:ulid-creator:5.2.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 }
--- common/common-domain/src/main/kotlin/net/spring/cloud/prototype/domain/converter/EventConverter.kt
+++ common/common-domain/src/main/kotlin/net/spring/cloud/prototype/domain/converter/EventConverter.kt
@@ -1,4 +1,13 @@
 package net.spring.cloud.prototype.domain.converter
 
+import com.fasterxml.jackson.databind.ObjectMapper
+
 class EventConverter {
+
+    companion object{
+        inline fun<reified T> fromEventString (objectMapper: ObjectMapper, payload:String) : T {
+            return objectMapper.readValue<T>(payload, T::class.java)
+        }
+    }
+
 }
