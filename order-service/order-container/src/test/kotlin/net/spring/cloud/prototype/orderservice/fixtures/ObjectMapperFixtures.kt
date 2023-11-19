e53a7df838407576a5c96abc98a5561aa6584cb9
chagchagchag <chagchagchag.dev@gmail.com>
2023-11-20 02:04:49 +0900

    order-service

--- order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/fixtures/ObjectMapperFixtures.kt
+++ order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/fixtures/ObjectMapperFixtures.kt
@@ -1,6 +1,5 @@
 package net.spring.cloud.prototype.orderservice.fixtures
 
-import com.fasterxml.jackson.databind.DeserializationConfig
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinFeature
