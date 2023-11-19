35715986e03b46051cdd3bd78fa8df11e7e6f0b1
chagchagchag <chagchagchag.dev@gmail.com>
2023-11-19 18:17:34 +0900

    order-service

--- order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingActionTest.kt
+++ order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingActionTest.kt
@@ -2,11 +2,13 @@ package net.spring.cloud.prototype.orderservice.eda_scenario_test.create_order
 
 import net.spring.cloud.prototype.domain.event.OutboxStatus
 import net.spring.cloud.prototype.domain.event.SagaStatus
+import net.spring.cloud.prototype.orderservice.config.properties.OrderServiceProperties
 import net.spring.cloud.prototype.orderservice.domain.factory.OrderCreatedEventFactory
 import net.spring.cloud.prototype.orderservice.domain.outbox.OrderOutboxHelper
 import net.spring.cloud.prototype.orderservice.domain.outbox.repository.OrderOutboxRepository
 import net.spring.cloud.prototype.orderservice.domain.publisher.OrderCreatedEventPublisher
 import net.spring.cloud.prototype.orderservice.fixtures.OrderOutboxEntityFixtures
+import net.spring.cloud.prototype.orderservice.kafka.KafkaStringProducer
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -15,6 +17,10 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.times
+import org.springframework.kafka.support.SendResult
+import java.util.concurrent.CompletableFuture
 
 @ExtendWith(MockitoExtension::class)
 class ScheduledPublishingActionTest {
@@ -22,12 +28,10 @@ class ScheduledPublishingActionTest {
     /**
      * OrderOutboxScheduler 는 주기적으로 OrderOutboxHelper 를 통해 publishOrderCreatedEvent() 를 호출한다.
      * - (SQL 검증) OrderOutboxRepository 의 findAllCreatedOrderEvent() 쿼리 검증
-     *  - domain/outbox/repository/OrderOutboxRepositoryTest 에 작성 완료
+     *   : domain/outbox/repository/OrderOutboxRepositoryTest 에 작성 완료
      * - (유효성 체크 검증) OrderOutboxEntity 의 payload 가 null 일때 IllegalArgumentException
      * - (유효성 체크 검증) OrderOutboxEntity 의 payload 가 공백문자일 때 IllegalArgumentException
      * - (전송 구문 호출 여부 검증) OrderCreatedEventPublisher 로 Kafka 메시지 큐에 데이터 전송 여부 검증
-     * - (전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증
-     * - (전송 실패 후 처리 결과값 검증) OutboxStatus 는 PENDING, SagaStatus 는 PENDING 검증
      */
 
     @Mock
@@ -38,6 +42,15 @@ class ScheduledPublishingActionTest {
 
     @Mock
     private lateinit var orderCreatedEventFactory: OrderCreatedEventFactory
+
+    @Mock
+    private lateinit var mockFuture: CompletableFuture<SendResult<String, String>>
+
+    @Mock
+    private lateinit var orderServiceProperties: OrderServiceProperties
+
+    @Mock
+    private lateinit var kafkaStringProducer: KafkaStringProducer
 
     @InjectMocks
     private lateinit var orderOutboxHelper: OrderOutboxHelper
@@ -87,7 +100,25 @@ class ScheduledPublishingActionTest {
     @Test
     @DisplayName("(전송 구문 호출 여부 검증) OrderCreatedEventPublisher 로 Kafka 메시지 큐에 데이터 전송 여부 검증")
     fun `(전송 구문 호출 여부 검증) OrderCreatedEventPublisher 로 Kafka 메시지 큐에 데이터 전송 여부 검증`(){
+        // given
+        val entityList = OrderOutboxEntityFixtures
+            .randomCreatedEntityList5()
 
+        // when
+        Mockito
+            .`when`(orderOutboxRepository.findAllCreatedOrderEvent(SagaStatus.CREATED, OutboxStatus.CREATED))
+            .thenReturn(entityList)
+
+        Mockito
+            .`when`(orderCreatedEventPublisher.sendEvent(any(), any()))
+            .thenReturn(mockFuture)
+
+        // then
+        orderOutboxHelper.publishOrderCreatedEvent()
+
+        Mockito
+            .verify(orderCreatedEventPublisher, times(5))
+            .sendEvent(any(), any())
     }
 
 }
--- order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingResultTest.kt
+++ order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingResultTest.kt
@@ -5,6 +5,11 @@ import org.junit.jupiter.api.Test
 
 class ScheduledPublishingResultTest {
 
+    /**
+     * - (전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증
+     * - (전송 실패 후 처리 결과값 검증) OutboxStatus 는 PENDING, SagaStatus 는 PENDING 검증
+     */
+
     @Test
     @DisplayName("(전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증")
     fun `(전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증`(){
