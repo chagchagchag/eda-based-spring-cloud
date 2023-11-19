fb144101775a699f8386f07e17a8070bb7f138f0
chagchagchag <chagchagchag.dev@gmail.com>
2023-11-19 17:39:37 +0900

    order-service

--- order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingActionTest.kt
+++ order-service/order-container/src/test/kotlin/net/spring/cloud/prototype/orderservice/eda_scenario_test/create_order/ScheduledPublishingActionTest.kt
@@ -90,16 +90,4 @@ class ScheduledPublishingActionTest {
 
     }
 
-    @Test
-    @DisplayName("(전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증")
-    fun `(전송 성공 후 처리 결과값 검증) OutboxStatus 는 FINISHED, SagaStatus 는 FINISHED 검증`(){
-
-    }
-
-    @Test
-    @DisplayName("(전송 실패 후 처리 결과값 검증) OutboxStatus 는 PENDING, SagaStatus 는 PENDING 검증")
-    fun `(전송 실패 후 처리 결과값 검증) OutboxStatus 는 PENDING, SagaStatus 는 PENDING 검증`(){
-
-    }
-
 }
