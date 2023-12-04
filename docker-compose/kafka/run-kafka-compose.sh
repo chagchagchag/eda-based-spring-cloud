1ee5fa5274e03b0bd3276634fbe032caa43121ce
chagchagchag <chagchagchag.dev@gmail.com>
2023-12-05 00:42:19 +0900

    refactoring/0.0.1
    docker network 및 docker-compose 정리 작업 중

--- docker-compose/kafka/run-kafka-compose.sh
+++ docker-compose/kafka/run-kafka-compose.sh
@@ -1,3 +1,12 @@
+cnt_foobar_network=`docker network ls --filter name=foobar-network | wc -l`
+cnt_foobar_network=$(($cnt_foobar_network -1))
+
+if [ $cnt_foobar_network -eq 0 ]
+then
+  echo "'foobar-network' 가 존재하지 않습니다. 'foobar-network'를 새로 생성합니다."
+  docker network create foobar-network
+fi
+
 docker-compose -f common.yml -f zookeeper.yml up -d
 docker-compose -f common.yml -f kafka_cluster.yml up -d
 #docker-compose -f common.yml -f kafka_connect.yml up -d
